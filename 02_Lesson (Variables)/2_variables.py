0dc1b6bfc470607e593006e93005b7e2639770ee
Technitute <nauman.hanif@kics.edu.pk>
2025-01-24 14:24:02 +0500

    Create 2_variables.py

---
+++ 02_Lesson (Variables)/2_variables.py
@@ -0,0 +1,20 @@
+# Data types
+# Text (String)
+# Decimal numbers (float)
+# Without Decimal numbers (integer)
+# True or False (Boolean)
+# Complex Numbers (comp)
+# List
+# Tuples
+# Dictionaries
+
+name = 'KAMRAN' # string variable
+age = 15 # integer
+height = 5.4 # float
+comp = 20 + 2j
+martial_status = True
+print('My name is:', name)
+print('My age is:', age)
+print('My height is:', height)
+print('My complex number is:', comp)
+print(type(comp))
