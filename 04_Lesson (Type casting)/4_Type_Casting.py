a10b7e9bebc917f8d5a14f6e64cc6d5d633835e0
Technitute <nauman.hanif@kics.edu.pk>
2025-01-24 14:27:58 +0500

    Create 4_Type_Casting.py

---
+++ 04_Lesson (Type casting)/4_Type_Casting.py
@@ -0,0 +1,16 @@
+# Type casting
+# int() --> to convert any data type to integer but not string alphabets
+# float() --> to convert any data type to float but not string alphabets
+# str() --> to convert any data type to string 
+# list() --> to convert any data type to list
+# tuple() --> to convert any data type to tuple
+# set() --> to convert any data type to set
+# dict() --> to convert any data type to dictionary
+
+n =  20
+print('The type of n:', type(n))
+f = float(n)
+print('The type of f:', type(f))
+h = 24
+string = str(h)
+print('The type of string:', type(string))
